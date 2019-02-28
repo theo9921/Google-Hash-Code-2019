94201cd153a1362e3c935eb1a3eb72af6a10c46b
Michael Collingwood <mike@fairways.me>
2019-02-28 19:37:58 +0000

    find_common_tags changed to take indexes which includes integers & tuples

--- michael/michael.py
+++ michael/michael.py
@@ -1,4 +1,5 @@
 def interest_factor_func(tags_1, tags_2):
+    """ CREATES INTEREST FACTOR FROM THE TAG LISTS OF THE TWO SLIDE CANDIDATES """
     interest_factor_1 = set(tags_1 + tags_2)   # Common tags
     
     intersect = tags_1 + tags_2
@@ -14,19 +15,38 @@ def interest_factor_func(tags_1, tags_2):
     return min([len(interest_factor_1), len(interest_factor_2), len(interest_factor_3)])
     
 
-def find_common_tags(tags_current, H):
+
+
+
+
+
+def find_common_tags(tags_current, INDEXES): # INDEXES represents every available slide that can occur =>> a tuple represents a pair of pictures for a vertical picture
+    """ TAKES LIST OF INDEXES OF PHOTOS (INC TUPLES REPRESENTING VERTICAL SLIDES)
+    AND THE CURRENT TAGS AND RETURNS A LIST OF INDEXES WHICH HAVE A TAG IN COMMON """
     global inputData
     
-    H_with_common_tags = []
+    INDEXES_with_common_tags = []
     # tags_current is a list of the tags of the current slide
     
-    for index in H:
+    
         
-        for tag in tags_current:
+    for tag in tags_current: # for each tag of the current slide
+        
+        for index in INDEXES:
             
-            if tag in inputData[index]: # assuming tag isn't H or a number
+            if type(index) == int:
                 
-                H_with_common_tags.append(index)
+                if tag in inputData[index]: # assuming tag isn't H, V or a number
+                    
+                    INDEXES_with_common_tags.append(index)
                 
-    return H_with_common_tags
+            else: # assuming tuple then
+                
+                for sub_index in index:
+                    
+                    if tag in inputData[sub_index]: 
+                    
+                        INDEXES_with_common_tags.append(index)
+                    
+    return INDEXES_with_common_tags
 
